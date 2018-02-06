10bb6c0ac5e18b315993eca08806a0b889c6ed2f
fonneimah <fonneimanjon@yandex.ru>
2018-02-06 20:00:25 +0300

    preview

--- Statistics_3.py
+++ Statistics_3.py
@@ -8,13 +8,7 @@ def deltatime(targettime, duration):
 	rawstr = regexp.search(targettime)
 	then = datetime.datetime(int(rawstr.group(1)), int(rawstr.group(2)), int(rawstr.group(3)))
 	delta = now - then
-	return (True if delta.days >= duration else False)
+	return delta
 
-old_issues = requests.get('https://api.github.com/search/issues?q=repo:googlechrome/puppeteer+type:issue+state:open&sort=created&per_page=100')
-regexp = re.compile(r'page=(\d+)>;\srel="last"')
-rawexp = regexp.search(old_issues.headers['Link'])
-lastpage = int(rawexp.group(1))
-for i in range(1, (lastpage + 1)):
-	data = requests.get('https://api.github.com/search/issues?q=repo:googlechrome/puppeteer+type:issue+state:open&sort=created&per_page=100&page=%d'%(i)).json()['items']
-	old_issnum = old_issnum + sum(list(map(lambda x: deltatime(x['created_at'], 14), data)))
-print("Old issues count: %d"%(old_issnum))
+old_issues = requests.get('https://api.github.com/search/issues?q=repo:googlechrome/puppeteer+created%3A%3C' + '2018-01-24' + '+state%3Aopen+is%3Aissue').json()['total_count']
+print(old_issues)
